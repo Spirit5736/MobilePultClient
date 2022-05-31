ded21d8d6c1ccabf2105b498cbcf7ad15e54aafa
ivk <ivan.keynes@bbconsult.co.uk>
2022-05-31 22:49:51 +0700

    Добавьте файлы проекта.

---
+++ Mobile-Pult-Client/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using MobilePultClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Mobile_Pult_Client
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Connect_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                TcpClient client = new TcpClient();
+                await client.ConnectAsync(IPAddress.Text, Convert.ToInt32(Port.Text));
+                if (client.Connected)
+                {
+                    Connection.Instance.client = client;
+                    Application.Current.MainPage = new NavigationPage(new OperationsPage());
+
+                    await DisplayAlert("Connected", "Connected to server successfully!", "Ok");
+                }
+                else
+                {
+                    await DisplayAlert("Error", "Connection unsuccessful!", "Ok");
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", "" + ex.ToString(), "Ok");
+            }
+        }
+
+    }
+}
